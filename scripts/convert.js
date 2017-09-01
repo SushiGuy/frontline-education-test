60776ab6810c86d9cc602eb8671f2ad2d627b4d2
SushiGuy <jeremyheiner@gmail.com>
2017-09-01 10:29:01 -0600

    Bonus Round phase 1
    Partly working

--- scripts/convert.js
+++ scripts/convert.js
@@ -8,48 +8,74 @@ $(function () {
     $("#startButton").on("click", function () {
         var inputString = $("#inputString").val();
         //Initialize global objects (prevents result duplicate stacking after first run)
-        gArray = [];
+        var gArray = [];
         gOutputString = "";
 
         //Trim off first paren
         inputString = inputString.substring(1, inputString.length);
-        convertString(inputString, 0, 0, "");
-        $("#output").html(gArray.join("\n"));
+        convertString(inputString, gArray, 0, 0, "");
+        displayArray(gArray);
     });
 });
 
 //"g" for global variable - declare outside of recursive loop to preserve state
 //var gOutputString;
 var gArray;
-var gBullet = "-";
+//var gBullet = "-";
 
-function convertString(str, i, arrDep, strCurr) {
-    while (i < str.length) {
-        //If a delimiter has been hit
-        if (str[i] == "(" || str[i] == "," || str[i] == ")") {
-            //If string builder has a value
-            if (strCurr.length > 0) {
-                //Push to output and reinitialize string builder
-                var bullet = (arrDep == 0 ? "" : gBullet.repeat(arrDep) + " ");
-                gArray.push(bullet + strCurr);
-                strCurr = "";
+function displayArray(gArray) {
+    var output = "";
+    for (i = 0; i < gArray.length; i++) {
+        if (gArray[i].constructor === Array) {
+            for (j = 0; j < gArray[i].length; j++) {
+                //output += gArray[i][j] + "\n";
+                output += gArray[i][j] + " - i/j = " + i + "/" + j + "\n";
             }
         }
+        else {
+            output += gArray[i] + " - i = " + i + "\n";
+        }
+    }
+    //$("#output").html(gArray.join("\n"));
+    $("#output").html(output);
+}
+
+function convertString(str, arr, i, arrDep, strCurr) {
+
+    // Loop for each major number (each row in the table)  
+    while (i < str.length) {
+
+        //If a delimiter has been hit 
+        if (str[i] == "(" || str[i] == "," || str[i] == ")") { 
+            //If string builder has a value 
+            if (strCurr.length > 0) { 
+                //Push to output and reinitialize string builder 
+                //var bullet = (arrDep == 0 ? "" : gBullet.repeat(arrDep) + " "); 
+                //gArray[arrDep] = strCurr;
+                //gArray.push(bullet + strCurr);
+                arr.push(strCurr);
+                strCurr = ""; 
+            } 
+        } 
 
         //If array start
         if (str[i] == "(") {
             arrDep++;
+            // Create a new array
+            var arr2 = arr[arrDep] = [];
+
             //Recurse
-            i = convertString(str, i + 1, arrDep, strCurr);
+            i = convertString(str, arr2, i + 1, arrDep, strCurr);
+
             //After recursion returns, decrement depth
             arrDep--;
         }
+
         //If array end, exit recursive call / loop
         else if (str[i] == ")") {
             return i + 1;
         }
-        else
-        {
+        else {
             //Aplhabetic character
             if (str[i] != ",") {
                 strCurr += str[i];
