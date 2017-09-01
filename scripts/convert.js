71c59a8089f3cbbd98782b79d4c7246e2d118374
SushiGuy <jeremyheiner@gmail.com>
2017-09-01 16:05:55 -0600

    Money! The right way
    Okay, so it's working the right way now with full recursion. Former "Bonus" check-ins were false alarms, moving to bonus round.

--- scripts/convert.js
+++ scripts/convert.js
@@ -7,89 +7,63 @@ $(function () {
 
     $("#startButton").on("click", function () {
         var inputString = $("#inputString").val();
-        //Initialize global objects (prevents result duplicate stacking after first run)
-        var outputArray = [];
-        //gOutputString = "";
+        // Initialize global objects (prevents result duplicate stacking after first run)
+        outputString = "";
 
-        //Trim off first paren (parens trigger new subarray, but we want to start with level 0)
+        // Trim off first paren (parens trigger next recursion, but we want to start with level 0)
         inputString = inputString.substring(1, inputString.length);
-        convertString(inputString, outputArray, 0, 0, "");
-        displayArray(outputArray);
+
+        // Main
+        convertString(inputString, 0, 0, "");
+
+        // Display results
+        $("#output").hide().html(outputString).fadeIn();
     });
 });
 
-//"g" for global variable - declare outside of recursive loop to preserve state
-//var gOutputString;
-//var gArray = [];
-//var gBullet = "-";
+var outputString;
+var bullet = "-";
 
-function displayArray(arr) {
-    var output = "";
-    var bullet = "-";
-    //var bullet = (arrDep == 0 ? "" : gBullet.repeat(arrDep) + " "); 
-    //debugger;
-    //var depth = 0;
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i].constructor === Array) {
-            //depth++;
-            for (j = 0; j < arr[i].length; j++) {
-                output += arr[i][j] + "\n";
-                //output += bullet + " " + arr[i][j] + "\n";
-                //output += arr[i][j] + " - i/j = " + i + "/" + j + " depth " + depth + "\n";
-            }
-        }
-        else {
-            output += arr[i] + "\n";
-            //output += arr[i] + " - i = " + i + "\n";
-        }
-    }
-    //$("#output").html(arr.join("\n"));
-    $("#output").html(output);
-}
-
-function convertString(str, arr, i, j, strCurr) {
+function convertString(str, i, j, strCurr) {
 
     // Loop for each major number (each row in the table)  
     while (i < str.length) {
 
-        //If a delimiter has been hit 
+        // If a delimiter has been hit 
         if (str[i] == "(" || str[i] == "," || str[i] == ")") { 
-            //If string builder has a value 
+            // If string builder has a value 
             if (strCurr.length > 0) { 
-                //Push to output and reinitialize string builder 
-                //var bullet = (arrDep == 0 ? "" : gBullet.repeat(arrDep) + " "); 
-                //gArray[arrDep] = strCurr;
-                //gArray.push(bullet + strCurr);
-                arr.push(strCurr);
+                // Push to output and reinitialize string builder 
+                // arr.push(strCurr);
+                var bull = (j == 0 ? "" : bullet.repeat(j) + " ");
+                outputString += bull + strCurr + "\n";
                 strCurr = ""; 
             } 
         } 
 
-        //If array start
+        // If sub array start
         if (str[i] == "(") {
             j++;
-            // Create a new array
-            var subArr = [];
-            arr.push(subArr);
 
-            //Recurse into subarray
-            i = convertString(str, subArr, i + 1, j, strCurr);
+            // Recurse into subarray
+            i = convertString(str, i + 1, j, strCurr);
 
-            //After recursion returns, decrement depth
+            // After recursion returns, decrement depth
             j--;
         }
-        //If array end, exit recursive call / loop
+        // If array end, exit recursive call / loop
         else if (str[i] == ")") {
             return i + 1;
         }
+        // No action on comma, could be inverted and swapped with next else to reduce code
         else if (str[i] == ",") {
-            //No action
         }
-        //Alphabetic character
+        // Alphabetic character
         else {
             strCurr += str[i];
         }
         i++;
     }
+
     return i;
 }
